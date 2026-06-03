ad385c66f269b3e76577efc6953fafe25b370d8b
Andy <andymilligan_mac@Mac.COhatch.com>
2026-06-03 16:00:50 -0400

    Fix Studio: add basePath /studio to sanity config

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   title: "Translation Tutorials",
   projectId: "9eiem856",
   dataset: "production",
+  basePath: "/studio",
   plugins: [structureTool(), visionTool()],
   schema: { types: schemaTypes },
 });
